035a737389225eea45101cd59890216eb7337493
Angel Maldonado <103060766+angel21985@users.noreply.github.com>
2022-08-22 23:59:38 -0300

    Add files via upload

---
+++ Dia1.py
@@ -0,0 +1,12 @@
+print("Bienvenido al generador de Nombres de Bandas!")
+
+print("Hola " + input("Cómo te llamás?\n"))
+
+ciudad_pais = input("Dónde naciste?\n")
+print(ciudad_pais)
+
+mascota = input("Decime el nombre de alguna de tus mascotas!\n")
+print(mascota)
+
+print("El nombre de la banda podría ser:\n")
+print(mascota + " " + "de" + " " + ciudad_pais)
